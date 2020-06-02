c81e6d8b7267f5a2db19df1dc4dc19705654ffc3
uji <ujiprog@gmail.com>
2020-06-03 01:25:10 +0900

    mod: remove MinPacUpdate

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 mkdir -p ~/dotfiles/vimrc/vim/pack/mypackage/opt
 git clone https://github.com/k-takata/minpac.git ~/dotfiles/vimrc/vim/pack/mypackage/opt/minpac
-vim +:qa
-vim +:MinPacUpdate
